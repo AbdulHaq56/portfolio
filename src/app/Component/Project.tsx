2a1f5580217053fa30df188047e3e42d474cea07
Abdul-Haq <159776568+karachiboy@users.noreply.github.com>
2024-09-16 10:54:55 +0500

    "Update project title"

--- src/app/Component/Project.tsx
+++ src/app/Component/Project.tsx
@@ -50,7 +50,7 @@ const Project = () => {
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h4 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    Coming Soon
+                    New Project
                   </h4>
                   <h2 className="title-font text-lg font-medium text-gray-900 mb-3">
                     Coming Soon
